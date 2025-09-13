d9b5ac32832d06fe4a3c7cd8624608b66698592e
mquang279 <quangm279@gmail.com>
2025-09-13 21:29:26 +0700

    feat: add endpoint to get all filename in bucket

--- src/main/java/com/example/demo/service/impl/MinioStorageServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/MinioStorageServiceImpl.java
@@ -3,16 +3,20 @@ package com.example.demo.service.impl;
 import com.example.demo.config.MinioConfig;
 import com.example.demo.exception.StorageException;
 import com.example.demo.service.MinioStorageService;
+import com.nimbusds.jose.shaded.gson.JsonObject;
+
 import io.minio.*;
 import io.minio.http.Method;
+import io.minio.messages.Item;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 public class MinioStorageServiceImpl implements MinioStorageService {
 
     private final MinioClient minioClient;
@@ -40,7 +44,6 @@ public class MinioStorageServiceImpl implements MinioStorageService {
             } else {
                 System.out.println("Bucket " + minioConfig.getBucketName() + " already existed!");
             }
-
         } catch (Exception e) {
             System.out.println("Errow when creating bucket" + minioConfig.getBucketName());
             throw new StorageException(e.getMessage());
@@ -53,7 +56,7 @@ public class MinioStorageServiceImpl implements MinioStorageService {
             String url = minioClient.getPresignedObjectUrl(
                     GetPresignedObjectUrlArgs.builder()
                             .method(Method.PUT)
-                            .bucket("uetjob")
+                            .bucket(minioConfig.getBucketName())
                             .object(folder + "/" + fileName)
                             .expiry(1, TimeUnit.DAYS)
                             .build());
@@ -61,5 +64,22 @@ public class MinioStorageServiceImpl implements MinioStorageService {
         } catch (Exception e) {
             throw new StorageException(e.getMessage());
         }
+    }
+
+    @Override
+    public List<String> getAllFilesNameInFolder(String folder) {
+        Iterable<Result<Item>> results = minioClient.listObjects(
+                ListObjectsArgs.builder().bucket(minioConfig.getBucketName()).prefix(folder + "/").build());
+        List<String> filesName = new ArrayList<>();
+
+        for (Result<Item> result : results) {
+            try {
+                Item item = result.get();
+                filesName.add(item.objectName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return filesName;
     }
 }
