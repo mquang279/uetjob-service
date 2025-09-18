b79ebc910fb683984da0ca975188a957a166fb53
mquang279 <quangm279@gmail.com>
2025-09-19 00:00:44 +0700

    feat: modifying loadUserByUsername method to get user role and permissions

--- src/main/java/com/example/demo/config/UserDetailCustom.java
+++ src/main/java/com/example/demo/config/UserDetailCustom.java
@@ -1,7 +1,10 @@
 package com.example.demo.config;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import com.example.demo.entity.Permission;
+import com.example.demo.entity.Role;
 import com.example.demo.service.UserService;
 
 @Component("userDetailsService")
@@ -26,7 +31,30 @@ public class UserDetailCustom implements UserDetailsService {
         return User.builder()
                 .username(user.getEmail())
                 .password(user.getPassword())
-                .authorities(user.getRole().getName())
+                .authorities(getAuthorities(user.getRole()))
                 .build();
+    }
+
+    private List<? extends GrantedAuthority> getAuthorities(
+            Role role) {
+        return getGrantedAuthorities(getPrivileges(role));
+    }
+
+    private List<String> getPrivileges(Role role) {
+
+        List<String> privileges = new ArrayList<>();
+        privileges.add(role.getName());
+        for (Permission permission : role.getPermissions()) {
+            privileges.add(permission.getName());
+        }
+        return privileges;
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String privilege : privileges) {
+            authorities.add(new SimpleGrantedAuthority(privilege));
+        }
+        return authorities;
     }
 }
